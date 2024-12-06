e68bd052dfe03de344aaa905469a2b579bebbf15
Dipan31Mondal <idipan2003@gmail.com>
2024-12-06 17:27:12 +0530

    genai analysis and admin dashboard done

--- AdminSection/views.py
+++ AdminSection/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from CustomerSection import mongo
 # Create your views here.
 
 
 def dashboard(request):
-    print("inside admin section")
-    print(request.user,request.user.is_admin)
+    json_data = mongo.get_analysis()
+    print(json_data)
+    data = [json_data['positive'], json_data['negative'], json_data['neutral']]
     if request.user.is_admin:
-        return render(request, 'dashboard.html')
+        return render(request, 'dashboard.html', {'data': data})
     else:
         return HttpResponse("<h1>Only Admins Can See This page </h1>")
